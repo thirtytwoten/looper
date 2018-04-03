7f76222da6e5303720d1d4cf1c79fa73c9634164
Polly Pocket <pp@lawn-128-61-32-71.lawn.gatech.edu>
2018-04-03 17:22:08 -0400

    var -> let

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
-var ExpressPeerServer = require('peer').ExpressPeerServer;
+let express = require('express');
+let app = express();
+let ExpressPeerServer = require('peer').ExpressPeerServer;
 
 app.get('/', function(req, res, next) { res.send('Hello world!'); });
 
-var server = app.listen(9000);
+let server = app.listen(9000);
 
-var options = {
+let options = {
     debug: true
 }
 
