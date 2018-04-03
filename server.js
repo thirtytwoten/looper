729504822027e1348550192bef9e3e8679d47fcd
Polly Pocket <pp@lawn-128-61-32-71.lawn.gatech.edu>
2018-04-03 17:47:58 -0400

    use handlebar view engine

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
+let path = require('path');
 let express = require('express');
-let app = express();
 let ExpressPeerServer = require('peer').ExpressPeerServer;
+let ehbs = require('express-handlebars');
 
-app.get('/', function(req, res, next) { res.send('Hello world!'); });
+let port = process.argv[2] || 9000;
+let app = express();
+let hbs = ehbs.create({
+  defaultLayout: 'main',
+  helpers: {},
+  partiailsDir: 'views/partials'
+});
 
-let server = app.listen(9000);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.render('central', {
+    // template vars
+  });
+});
+
+let server = app.listen(port);
 
 let options = {
     debug: true
