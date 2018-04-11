9de0bc036bf0165f8d2500cd3b1baf4c911d930d
thirtytwoten <thirtytwoten3210@gmail.com>
2018-04-11 02:36:10 -0400

    add station class to station manager

--- StationManager.js
+++ StationManager.js
@@ -1,5 +1,36 @@
 let stations = [];
 
+class Station {
+  constructor(name, connectedPeers){
+    this.id = name;
+    this.name = name;
+    this.connectedPeers = connectedPeers;
+    this.createdAt = Date.now();
+  }
+
+  host(){
+    return connectedPeers[0];
+  }
+
+  age(){
+    return Date.now() - this.createdAt;
+  }
+
+  join(nodeId) {
+    this.connectedPeers.push(nodeId);
+  }
+
+  leave(nodeId) {
+    let i = this.connectedPeers.indexOf(nodeId);
+    this.connectedPeers.splice(i,1);
+    return this.connectedPeers.length;
+  }
+}
+
+function createStation(name, connectedPeers) {
+  stations.push(new Station(name, connectedPeers));
+}
+
 function getStation(stationId){
   return stations.find((s) => s.id === stationId);
 }
@@ -8,20 +39,20 @@ function getIndex(stationId){
   return stations.findIndex((s) => s.id === stationId);
 }
 
-function addStation(station) {
-  if(getIndex(station.id) > 0){
-    // station with that id already exists
-    return 0;
-  } else {
-    stations.push(station);
-    return 1;
-  }
-  
-}
-
 function removeStation(stationId) {
   stations.splice(this.getIndex(stationId), )
   delete getStation(stationId);
+}
+
+function joinStation(nodeId, stationId) {
+  getStation(stationId).join(nodeId);
+}
+
+function leaveStation(nodeId, stationId) {
+  let station = getStation(stationId);
+  if (station.leave(nodeId) < 1) {
+    removeStation(stationId);
+  }
 }
 
 module.exports = {
